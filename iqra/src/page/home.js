14e77187b77c53d4c548cbff25e3dda5ee54b52f
universestar2023 <marufamansuri9@gmail.com>
2024-08-08 19:33:20 +0530

    mcomit

--- iqra/src/page/home.js
+++ iqra/src/page/home.js
@@ -4,6 +4,7 @@ function HomePage() {
   return (
     <div className="home-page">
       <h1>Welcome to Our Website!</h1>
+      <h3>hiii</h3>
       <p>This is the homepage content. You can add more text, images, and components here.</p>
     </div>
   );

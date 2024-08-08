bfdbfacd28b7e3bb78010530e3158f6c2e55f631
universestar2023 <marufamansuri9@gmail.com>
2024-08-09 00:47:40 +0530

    mc3

--- iqra/src/App.js
+++ iqra/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Features from './page/Features.jsx';
-import HomePage from './page/home';
+import HomePage from './page/homes';
+import Navbar from './page/navbar.jsx';
 
 function App() {
   return (
     <>
+    <Navbar/>
     <HomePage/>
     <Features/>
     </>

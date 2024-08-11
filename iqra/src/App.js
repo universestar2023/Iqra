3ef5fb58ab67d7e9c7191d0ce956b3ada0e3e72b
kash2004 <kashmini.meshram434@gmail.com>
2024-08-11 14:43:07 +0530

    Last Page Added

--- iqra/src/App.js
+++ iqra/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Features from './page/Features.jsx';
 import HomePage from './page/home';
+import LastPage from './LastPage/lastpage.js';
 
 function App() {
   return (
     <>
     <HomePage/>
     <Features/>
+    <LastPage/>
     </>
   );
 }
